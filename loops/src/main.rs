1bbef745d60f4c9a959d44f9365ff987e33a0d70
Ishan Sharma <ishansharma@Ishans-MacBook-Air.local>
2023-09-18 21:58:51 +0530

    Fibonnaci

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
+    // basic_loop();
+    // label_loops();
+    fibonacci();
+}
+
+fn _basic_loop() {
     let mut counter = 0;
     
     let result = loop {
@@ -9,10 +15,23 @@ fn main() {
 
     };
     println!("Value of result is {result}");
-    label_loops();
 }
 
-fn label_loops() {
+fn fibonacci() {
+    let mut curr = 1;
+    let mut next = 1;
+    let mut temp : i32;
+    let mut num = 1;
+    while num < 20 {
+        println!("{num} Fibonacci number is : {curr}");
+        temp = curr + next;
+        curr = next;
+        next = temp;
+        num += 1;
+    }
+}
+
+fn _label_loops() {
     let mut count = 0;
     // Can add labels like this and refer these while calling break / continue
     'counting_up: loop {
@@ -35,6 +54,11 @@ fn label_loops() {
     while count != 3 {
         println!("End count = {count}");
         count += 1
+    }
+
+    let a : [i32; 5] = [1,2,3,4,5];
+    for element in a {
+        println!("The value is : {element}");
     }
     
 }
